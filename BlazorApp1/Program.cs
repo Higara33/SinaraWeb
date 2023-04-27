eadd156ba0faf5f8a249cb4aa333e517e4ff5b39
Андрей Кузьмин <duha3222@gmail.com>
2023-04-27 10:28:18 +0200

    save copy

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,11 +1,14 @@
 using BlazorApp1;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sinara.UserService.TransortTypes.Api;
+using Sinara.UserService.TransortTypes.Api.Contracts;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IUsersHttpApi, UsersHttpApi>();
 
 await builder.Build().RunAsync();
