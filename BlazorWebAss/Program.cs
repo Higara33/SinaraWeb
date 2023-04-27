c9f3723f405b10a4b7bfda5d2b3e9ec7567943e2
Андрей Кузьмин <duha3222@gmail.com>
2023-04-27 11:01:20 +0200

    reCreate blazor app

--- BlazorWebAss/Program.cs
+++ BlazorWebAss/Program.cs
@@ -1,14 +1,11 @@
-using BlazorApp1;
+using BlazorWebAss;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Sinara.UserService.TransortTypes.Api;
-using Sinara.UserService.TransortTypes.Api.Contracts;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped<IUsersHttpApi, UsersHttpApi>();
 
 await builder.Build().RunAsync();
