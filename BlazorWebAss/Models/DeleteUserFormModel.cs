c1484910b08535de03bc7b0b81586a5d29f246eb
Андрей Кузьмин <duha3222@gmail.com>
2023-05-01 13:44:54 +0200

    validating

---
+++ BlazorWebAss/Models/DeleteUserFormModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAss.Models
+{
+    public class DeleteUserFormModel
+    {
+        [Required(ErrorMessage = "Login is required field")]
+        public string Login { get; set; } = string.Empty;
+    }
+}
--- BlazorWebAss/Models/EditUserFormModel.cs
+++ BlazorWebAss/Models/EditUserFormModel.cs
@@ -1,11 +1,18 @@
-﻿namespace BlazorWebAss.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAss.Models
 {
     public class EditUserFormModel
     {
         public string FirstName { get; set; } = string.Empty;
+
         public string LastName { get; set; } = string.Empty;
+
         public string FatherName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Login is required field")]
         public string Login { get; set; } = string.Empty;
+
         public string NewLogin { get; set; } = string.Empty;
     }
 }
